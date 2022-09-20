695f2fd9796b91a572801c6059d9ad6c2e9badf5
Isaac Ramirez Herrera <joseisaacr@microsoft.com>
2022-09-20 10:26:53 -0600

    Fixes

--- src/Morph.ts
+++ src/Morph.ts
@@ -1,40 +1,20 @@
 import fs from 'fs';
-import ts from 'typescript';
-import { Project, ScriptTarget, Statement, StructureKind, Node } from "ts-morph";
-import { openFileForWrite } from './io/FileWriter';
-import { isGoogProvide } from './transformation/rules/goog-provide-rule';
+import { Project, ScriptTarget } from "ts-morph";
+import { transformGoogProvideToNamespace } from './transformation/TransformGoogProvideToNamespace';
 
-const project = new Project({
+(async () => {
+  const project = new Project({
     compilerOptions: {
-      target: ScriptTarget.ESNext
+      target: ScriptTarget.ES2022
     },
   });
 
-let sourceFile = project.createSourceFile("../shaka-player-fork/lib/util/error.ts", 
+  const sourceFile = project.createSourceFile("../shaka-player-fork/lib/util/error.ts", 
     fs.readFileSync('../shaka-player-fork/lib/util/error.js', 'utf8'),
-    {
-        overwrite: true        
-    });
-    let toRemove: number | undefined;
-    // sourceFile.getStatements().forEach((statement, index) => {
-    //     if (statement.getText().includes('shaka.util.Error')) {
-    //         toRemove = index;
-    //     }
-    // });
-    if (toRemove) {
-        sourceFile = sourceFile.removeStatement(toRemove);
-    }
-    
-// sourceFile.transform(traversal => {
-//     const node = traversal.visitChildren(); // return type is `ts.Node`
-//     if (isGoogProvide(node)) {
-//         toDelete = node;
-//     }
-//     // if (ts.isNumericLiteral(node)) {
-//     //   const incrementedValue = parseInt(node.text, 10) + 1;
-//     //   return ts.createNumericLiteral(incrementedValue.toString());
-//     // }
-//     return node;
-// });
-// sourceFile.getStatement()
-sourceFile.save();
+    { overwrite: true });
+
+  transformGoogProvideToNamespace(sourceFile);
+
+  sourceFile.save();
+})();
+
---
+++ src/transformation/TransformGoogProvideToNamespace.ts
@@ -0,0 +1,45 @@
+import ts, { CallExpression } from "typescript";
+import { SourceFile, Node, ExpressionStatement } from "ts-morph"
+
+export const transformGoogProvideToNamespace = (sourceFile: SourceFile) => {
+    const sourceText = sourceFile.getFullText();
+    const moduleName = removeGoogProvide(sourceFile);
+    if (!moduleName) {
+        throw new Error('Unable to find goog.provide statement');
+    }
+    sourceFile.removeText();
+    sourceFile.addModule({
+        name: moduleName,
+        statements: sourceText,    
+    });
+}
+
+const removeGoogProvide = (sourceFile: SourceFile): string | undefined => {
+    let googProvideModuleName = '';
+    const found = sourceFile.forEachDescendant((node: Node<ts.Node>) => {
+        if (isGoogProvide(node.compilerNode)) {     
+            const statement = node as ExpressionStatement;       
+            googProvideModuleName = getGoogProvideModuleName(statement.compilerNode)
+            statement.remove();
+            return true;
+        }
+    });
+    if (found) {
+        return googProvideModuleName;
+    }
+    return undefined;
+};
+
+const isGoogProvide = (node: ts.Node): boolean => {
+    if (ts.isExpressionStatement(node) && ts.isCallExpression(node.expression) && ts.isPropertyAccessExpression(node.expression?.expression)) {
+        const propertyAccessExpression = node.expression?.expression;
+        return propertyAccessExpression.getText() === 'goog.provide';
+    }
+    return false;
+};
+
+const getGoogProvideModuleName = (node: ts.Node): string => {
+    const expressionStatement = node as ts.ExpressionStatement;
+    const argumentList = (expressionStatement.expression as CallExpression).arguments;
+    return argumentList[0].getText();
+};
